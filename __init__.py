17c874c1e0a42fcbc40e43222652c18e0d7cd0b2
CowBus <103305540+RushiKumthekar@users.noreply.github.com>
2026-09-23 13:37:38 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from dagster import Definitions
-from .assets import web_scraped_data, mongodb_data, cleaned_data, store_to_postgres
-
-# Define Dagster Definitions
-defs = Definitions(
-    assets=[web_scraped_data, mongodb_data, cleaned_data, store_to_postgres]
-)
